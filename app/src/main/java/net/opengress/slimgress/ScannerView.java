101cb5c212adc12c6716c9c5f37facd66ecb2841
Chris <git@chris-nz.com>
2024-07-20 21:24:58 +1200

    build size optimisations and modernisations

--- app/src/main/java/net/opengress/slimgress/ScannerView.java
+++ app/src/main/java/net/opengress/slimgress/ScannerView.java
@@ -37,7 +37,7 @@ import java.util.Set;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
-import android.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
