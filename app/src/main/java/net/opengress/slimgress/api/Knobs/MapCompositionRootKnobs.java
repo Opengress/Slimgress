63b9a2e77018f8762d84ca51919fcc8ac5f664cf
Chris <git@chris-nz.com>
2024-10-25 23:34:16 +1300

    Allow switching map sources (raster only) with new map library

--- app/src/main/java/net/opengress/slimgress/api/Knobs/MapCompositionRootKnobs.java
+++ app/src/main/java/net/opengress/slimgress/api/Knobs/MapCompositionRootKnobs.java
@@ -21,6 +21,7 @@
 
 package net.opengress.slimgress.api.Knobs;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -33,6 +34,8 @@ public class MapCompositionRootKnobs extends Knobs {
     public static class MapProvider {
 
         private final String mName;
+        private final MapType mType;
+        private final String mAttribution;
         private final String[] mBaseUrls;
         private final String mFilenameEnding;
         private final int mMinZoom;
@@ -45,8 +48,19 @@ public class MapCompositionRootKnobs extends Knobs {
             TMS
         }
 
+        public enum MapType {
+            RASTER,
+            VECTOR
+        }
+
         public MapProvider(JSONObject json) throws JSONException {
             mName = json.getString("name");
+            if (json.getString("type").equals("raster")) {
+                mType = MapType.RASTER;
+            } else {
+                mType = MapType.VECTOR;
+            }
+            mAttribution = json.getString("attribution");
             var baseUrls = json.getJSONArray("baseUrls");
             ArrayList<String> urls = new ArrayList<>();
             for (int x = 0; x < baseUrls.length(); x++) {
@@ -66,6 +80,14 @@ public class MapCompositionRootKnobs extends Knobs {
 
         public String getName() {
             return mName;
+        }
+
+        public MapType getType() {
+            return mType;
+        }
+
+        public String getAttribution() {
+            return mAttribution;
         }
 
         public int getMinZoom() {
@@ -92,6 +114,56 @@ public class MapCompositionRootKnobs extends Knobs {
             return mCoordinateSystem;
         }
 
+        public String getStyleJson() throws JSONException {
+            var provider = this;
+            JSONObject source = new JSONObject();
+            source.put("type", provider.getType() == MapType.RASTER ? "raster" : "vector");
+
+            // Define tiles array based on base URLs and filename ending
+            JSONArray tilesArray = new JSONArray();
+            for (String baseUrl : provider.getBaseUrls()) {
+                tilesArray.put(baseUrl + "{z}/{x}/{y}" + provider.getFilenameEnding());
+            }
+
+            source.put("tiles", tilesArray);
+            source.put("minzoom", provider.getMinZoom());
+            source.put("maxzoom", provider.getMaxZoom());
+            source.put("tileSize", provider.getTileSize());
+            source.put("attribution", provider.getAttribution());
+
+            // Conditionally set the scheme to either "xyz" or "tms"
+            if (provider.getCoordinateSystem() == CoordinateSystem.TMS) {
+                source.put("scheme", "tms");
+            } else {
+                source.put("scheme", "xyz");  // Explicitly set for clarity
+            }
+
+            // Create the sources object
+            JSONObject sources = new JSONObject();
+            sources.put(provider.getName().toLowerCase().replace(" ", "-"), source);
+
+            // Create the layer JSON
+            JSONObject layer = new JSONObject();
+            layer.put("id", provider.getName().toLowerCase().replace(" ", "-") + "-layer");
+            layer.put("type", "raster");
+            layer.put("source", provider.getName().toLowerCase().replace(" ", "-"));
+            layer.put("minzoom", provider.getMinZoom());
+            layer.put("maxzoom", provider.getMaxZoom());
+
+            // Create the layers array
+            JSONArray layersArray = new JSONArray();
+            layersArray.put(layer);
+
+            // Create the final style JSON
+            JSONObject styleJson = new JSONObject();
+            styleJson.put("version", 8);
+            styleJson.put("name", provider.getName());
+            styleJson.put("sources", sources);
+            styleJson.put("layers", layersArray);
+
+            return styleJson.toString();
+        }
+
     }
 
     private final HashMap<String, MapProvider> mMapProviders;
@@ -109,7 +181,7 @@ public class MapCompositionRootKnobs extends Knobs {
         }
     }
 
-    public MapProvider fromString(String name) {
+    public MapProvider getMapProvider(String name) {
         if (Objects.equals(name, "default")) {
             return mMapProviders.entrySet().iterator().next().getValue();
         }
